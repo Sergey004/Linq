68c8403e33e2edb88a1f8f0ccc31fb5968518fdf
Sergey Grigoriev <sergey00475@gmail.com>
2020-04-22 10:44:28 +0400

    Добавьте файлы проекта.

---
+++ Linq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSet set = new TestSet();
+
+            //var result = set.Where(i => i <=5).Aggregate(1, (acc, i) => acc * i);
+            //var result = set.Aggregate(0, (acc, i) => acc + i);
+            //var result = set.First(s => s % 2 == 0);
+            //var resultSet = set.Where(s => s >100).Select(s => "*" + s.ToString() + "*");
+            //foreach (string i in resultSet)
+            //{
+            //    Console.WriteLine(i);
+            // }
+            var resultSet = set.Select(i => new { Numder = i, IsEven = i % 2 == 0 }).OrderBy(r => r.IsEven);
+
+            foreach (var element in resultSet)
+            {
+                Console.WriteLine(element.Numder);
+            }
+            //Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Linq/TestSet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class TestSet : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        { 
+            for (int i =1; i <=100;i++)
+            {
+                yield return i;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+        
+}
